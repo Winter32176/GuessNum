62ba4ab68c5916834380e65d733265b2038c9b60
JavaCourses1 <robert321766@outlook.com>
2020-02-11 20:57:37 +0200

    ddddddd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,85 @@
 package com.company;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
         Random random = new Random();
+        String ABC="Yes";
 
-        System.out.println("What is your name?");
-        String name = scan.next();
-        System.out.println("Hello, " + name + "!");
-        System.out.println("Guess the number from 1-100");
 
-        int myNumber = random.nextInt(100) + 1;
-        System.out.println("Cheat: " + myNumber);
+        do {
+            System.out.println("What is your name?");
+            String name = scan.next();
+            System.out.println("Hello, " + name + "!");
+            System.out.println("Guess the number from 1-100");
 
-        for (int i = 0; i < 11; i++) {
-            System.out.println("Enter your guess");
-            int userNumber = scan.nextInt();
+            int myNumber = random.nextInt(100) + 1;
+            System.out.println("Cheat: " + myNumber);
 
-            if (myNumber != userNumber) {
-                System.out.println("Try again");
 
-                if (myNumber < userNumber) {
-                    System.out.println("Your number is bigger");
-                } else {
-                    System.out.println("Your number is less");
+            for (int i = 0; i < 10; i++) {
+                int userNumber = askGuess();
+
+
+                if (myNumber > userNumber) {
+                    if (i < 9) System.out.println("Your number is less");
+                } else if (myNumber < userNumber) {
+                    if (i < 9) System.out.println("Your number is bigger");
+                } else if (myNumber == userNumber) {
+                    System.out.println("Congratulations, you win " + name + "!");
+                    QagainORnot();
+
+                }
+                if (i == 9) {
+                    System.out.println("You lose " + name + "!");
+                    System.out.println("Number was: " + myNumber);
                 }
 
             }
+        } while(ABC.equals("Yes"));
 
-            if (myNumber == userNumber) {
-                System.out.println("Congralulations, you win " + name + "!" );
-                break;
+    }
 
+    static int askGuess() {
+        for (; ; ) {
+            try {
+                System.out.println("Enter your guess");
+                int num = scan.nextInt();
+                if (num >= 1 & num <= 100) {
+                    return num;
+                } else {
+                    System.out.println("Enter the number from 1-100");
+                }
+
+
+            } catch (InputMismatchException wrong_input) {
+                String str = scan.next();
+                System.out.println(str + " isn't number");
 
             }
 
         }
+    }
+
+    static String QagainORnot() {
+        for (; ; ) {
+            System.out.println("Again?");
+            String ABC = scan.next();
+
+            if (ABC.equals("No")) {
+                return ABC;
+            } else if (ABC.equals("Yes")) {
+                return ABC;
+            } else {
+                System.out.println("Enter Yes or No");
+            }
 
 
+        }
     }
 }
